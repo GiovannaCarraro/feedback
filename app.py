ffac0bf1df4bbee596e547adea4a0810db79b9d7
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-01-30 17:22:09 -0300

    add: sextou

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,8 +6,27 @@ app = Flask(__name__)
 def pagina_principal():
     return render_template("principal.html")
 
-@app.route("/Sobre")
+@app.route("/Sobre", methods=["GET"])
 def pagina_sobre():
-    return render_template("/sobre.html")
+    return render_template("sobre.html")
+
+@app.route("/Login", methods=["GET"])
+def pagina_login():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+   usuario = request.form.get("nome")
+   senha = request.form.get("senha")
+
+   if usuario == "godofredo" and senha == "godofredo123":
+    return "Você acessou a pagina"
+   else:
+      return render_template ("login.html", erro = "acesso negado")
+   
+@app.route("/Comentarios")
+def pagina_comentarios():
+   return render_template("comentarios.html")
+
 
 app.run(debug=True)
