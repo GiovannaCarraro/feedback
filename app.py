e27bf1eb1d05ce4a1a2147d3b382619fac25678e
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-02-06 15:50:35 -0300

    MODIFY: requirements

--- app.py
+++ app.py
@@ -39,4 +39,5 @@ def adicionar_comentario():
    print(lista_comentarios)
    return redirect("/comentarios")
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
